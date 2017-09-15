cc9b824a42680a2e8c432909e2e9f476c52674b4
Dilshat Hewzulla <hewzulla@gmail.com>
2017-09-15 19:48:41 +0100

    update

--- deploy/br.sh
+++ deploy/br.sh
@@ -1,5 +1,5 @@
 mkdir build
 
-browserify  -r ./lib/index.js:global-input-message > build/global-input-message.js
+browserify  -r ./lib/index.js:global-input-message > lib/global-input-message.js
 ssh root@globalinput.co.uk 'mkdir global-input-node/nginx/data/websites/globalinput/lib'
 scp build/global-input-message.js root@globalinput.co.uk:global-input-node/nginx/data/websites/globalinput/lib

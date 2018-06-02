612a62e1f7733c2e5f2033b00d46259f2aa3117e
Dilshat Hewzulla <hewzulla@gmail.com>
2018-06-02 16:33:33 +0100

    es5 transpiler

--- deploy/localdeploy.sh
+++ deploy/localdeploy.sh
@@ -1,8 +1,8 @@
 mkdir ../globalInputMobile/app/global-input-message
-rsync -azvv src/ ../globalInputMobile/app/global-input-message/
+rsync -azvv distribution/ ../globalInputMobile/app/global-input-message/
 
 mkdir ../global-input-web/app/global-input-message/src
-rsync -azvv src/ ../global-input-web/src/global-input-message/
+rsync -azvv distribution/ ../global-input-web/src/global-input-message/
 
 
 #mkdir ~/box/image-client-app/src/global-input-messafe

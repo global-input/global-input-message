f31c2e0cfa33d3a9fa8e339a456048dc784b5849
Dilshat Hewzulla <hewzulla@gmail.com>
2019-09-08 17:49:34 +0100

    babel configurtation

--- babel.config.js
+++ babel.config.js
@@ -7,14 +7,11 @@ const presets = [
           firefox: "60",
           chrome: "67",
           safari: "11.1",
-        }        
-      },
-    ],
+        },
+        useBuiltIns:"usage"
+      }
+      
+      ],
   ];
   
-  module.exports = { presets:[
-       ["@babel/preset-env"
-      ]
-
-
-  ] };
+  module.exports = { presets:presets};

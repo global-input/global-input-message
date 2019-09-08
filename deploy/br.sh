0f0e37c92148dbe35401800433ef4e773c26a476
Dilshat Hewzulla <hewzulla@gmail.com>
2019-09-08 12:45:44 +0100

    deleted the not used files

--- deploy/br.sh
+++
@@ -1,5 +0,0 @@
-mkdir build
-
-browserify  -r ./lib/index.js:global-input-message > lib/global-input-message.js
-ssh root@globalinput.co.uk 'mkdir global-input-node/nginx/data/websites/globalinput/lib'
-scp build/global-input-message.js root@globalinput.co.uk:global-input-node/nginx/data/websites/globalinput/lib

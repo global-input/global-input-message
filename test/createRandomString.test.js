7a7644079a2bc225aa0daa19033f3672b4689317
Dilshat Hewzulla <hewzulla@gmail.com>
2021-02-22 09:27:36 +0000

    random-string-test

---
+++ test/createRandomString.test.js
@@ -0,0 +1,25 @@
+
+import { generateRandomString } from "../src";
+
+
+
+
+describe("Communications between a Mobile App and a Device App", () => {
+
+    test('tests for messages sent between a mobile app and a device app', () => {
+        const stringLength = 20;
+        const generatedStrings = [];
+        for (let i = 0; i < 1000; i++) {
+            const randomString1 = generateRandomString(stringLength);
+            console.log(randomString1);
+            expect(randomString1.length).toBe(stringLength);
+            const duplicates = generatedStrings.filter(f => f === randomString1);
+            //expect(duplicates.length).toBe(0);
+            generatedStrings.push(randomString1);
+        }
+
+
+    });
+
+
+});
